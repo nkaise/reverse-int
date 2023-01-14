fa5c4a5d661e801f6656f3c8a80ed6e110bb644f
Anastasia Ganyushkina <kaise95@gmail.com>
2023-01-14 12:16:55 +0400

    feat: js code add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
-  
+    var numString = n.toString();
+    if (numString[0] === '-') {
+        var num = '';
+        for (let i = numString.length - 1; i >= 1; i--) {
+            num += numString[i];
+        }
+        return num
+    }
+    if (numString[numString.length-1] === '0') {
+        var num = '';
+        for (let i = numString.length - 2; i >= 0; i--) {
+            num += numString[i];
+        }
+        return num
+    }
+    else {
+        var num = '';
+        for (let i = numString.length - 1; i >= 0; i--) {
+            num += numString[i];
+        }
+        return num
+    }
 }
